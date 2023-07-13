60afab6df8e4d8ef0ac90c23f03f30207d971551
KEVIN MISTRY <114051869+KevinMistry03@users.noreply.github.com>
2023-07-13 14:01:48 +0530

    make slider responsive again

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -35,20 +35,6 @@ const Home = () => {
         el: '.swiper-pagination',
         clickable: true,
       },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-      },
     });
 
 
